5adfaaf1dd872345cb7671efab10e8aff58356c8
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-03-19 12:38:25 -0700

    Raise error and fix code

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 def select_dish(foods, selected_food):
+    if selected_food < 0:
+        raise ValueError
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
 def your_menu(foods):
-    try:
-        index = 1
-        for dish in foods:
-            print(f"{index}. {dish}")
-            index += 1
-    
-        selected_choice = int(input("Your order number? "))
-        select_dish(foods, selected_choice - 1)
-    except IndexError as error:
-        print(f"{error} was entered.")
-        print("Next time try entering something on the menu!")
+    while True:
+        try:
+            index = 1
+            for dish in foods:
+                print(f"{index}. {dish}")
+                index += 1
+            selected_choice = int(input("Your order number? "))
+            #if selected_choice - 1 < 0:
+            #    raise ValueError
+            select_dish(foods, selected_choice - 1)
+            print("Finished")
+            break
+        except (IndexError, ValueError) as error:
+            print(f"{error} was entered.")
+            print("Next time try entering something on the menu!")
+            print("Invalid input! Please enter a valid number.")
 
 menu_items = [
     "Yakisoba",
@@ -24,3 +31,7 @@ menu_items = [
 
 your_menu(menu_items)
 print("Yum!")
+
+# ValueError: invalid literal for int() with base 10: 'g'
+# Negative Index or Zero
+# IndexError
